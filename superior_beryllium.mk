f289a3700e3279f195f3569f4667411877d263eb
Tailanbr <tailansantos50@hotmail.com>
2021-12-04 01:18:31 +0000

    beryllium: superior bringup

---
+++ superior_beryllium.mk
@@ -0,0 +1,31 @@
+#
+# Copyright (C) 2018-2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+$(call inherit-product, device/xiaomi/beryllium/device.mk)
+
+# Inherit some common SuperiorOS stuff.
+$(call inherit-product, vendor/superior/config/common.mk)
+
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# Device identifier. This must come after all inclusions.
+DEVICE_MAINTAINER := TailanCunha
+PRODUCT_NAME := superior_beryllium
+PRODUCT_DEVICE := beryllium
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := POCO F1
+PRODUCT_MANUFACTURER := Xiaomi
+
+BUILD_FINGERPRINT := "google/redfin/redfin:12/SPB5.210812.002/7671067:user/release-keys"
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="redfin-user 12 SPB5.210812.002 7671067 release-keys" \
+    PRODUCT_NAME="beryllium"
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.build.fingerprint=google/redfin/redfin:12/SPB5.210812.002/7671067:user/release-keys
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
